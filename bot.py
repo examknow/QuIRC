89e1a2c5f1ed2c363a40153ab3ce7a9bda48571a
RhinosF1 <46229976+RhinosF1@users.noreply.github.com>
2019-05-18 10:38:49 +0100

    Add quotebot (#15)
    Adding quotebot to beta V1

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import re
 import time
 import random
 topic = "General Chat - Logged at http://wm-bot.wmflabs.org/logs/%23%23RhinosF1/?C=M;O=D , Enjoy - if you don't, have a good one | Rules and Info - https://pastebin.com/C2brE9bh"
-nick = quirctest123
+nick = 'quirctest123'
 bot = QuIRC.IRCConnection()
 greetings = [
     "Hello {}!",
@@ -23,6 +23,7 @@ def on_welcome(bot):
     bot.send_message('NickServ', 'identify ')
     print('Authed to NickServ')
     time.sleep(10)
+    bot.join_channel('##test1')
 def on_message(bot, channel, sender, message):
     if "hi" in message.lower() or "hello" in message.lower():
         greeting_message = random.choice(greetings).format(sender)
@@ -37,7 +38,7 @@ def on_message(bot, channel, sender, message):
         if len(message.split()) > 1:
             location = message.lower()
             location = location[9:]
-            apikey = #yourapikeykere
+            apikey = 'none' #yourapikeykere
             weather_data = requests.get("http://api.openweathermap.org/data/2.5/weather?q="+location+"&APPID="+apikey+ "&units=metric").json()
             if weather_data["cod"] == 200:
                 bot.send_message(channel, "The weather in {} is {} and {} degrees.".format(weather_data["name"], weather_data["weather"][0]["description"], weather_data["main"]["temp"]))
@@ -45,6 +46,18 @@ def on_message(bot, channel, sender, message):
                 bot.send_message(channel, sender + ': API Fault')
         else:
             bot.send_message(channel, "Usage: !weather Istanbul")
+    for message_part in message.split():
+        if message_part.startswith("!pickquote"):
+            numq = message.lower()
+            numq = numq[10:]
+            quotelist = open('quotes.csv', 'r')
+            quotes = quotelist.read()
+            quotes = quotes.split(',')
+            numq = int(numq)-1
+            picked = random.randint(0,int(numq))
+            pq = quotes[picked]
+            bot.send_message(channel, 'Todays quote is: ' + str(pq))
+            bot.send_message('ChanServ', 'topic '+ channel + ' topic ' + ' | Quote of the day: ' + pq)
 
 bot.on_connect.append(on_connect)
 bot.on_welcome.append(on_welcome)
