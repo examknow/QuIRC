b27acc520157259de9595318d380a969c11a088c
RhinosF1 <46229976+RhinosF1@users.noreply.github.com>
2019-05-18 10:13:58 +0100

    Add weather bot

--- bot.py
+++ bot.py
@@ -33,6 +33,18 @@ def on_message(bot, channel, sender, message):
             title_match = re.search("<title>(.*?)</title>", html)
             if title_match:
                 bot.send_message(channel, "Title of the URL by {}: {}".format(sender, title_match.group(1)))
+    if message.split()[0] == "!weather":
+        if len(message.split()) > 1:
+            location = message.lower()
+            location = location[9:]
+            apikey = #yourapikeykere
+            weather_data = requests.get("http://api.openweathermap.org/data/2.5/weather?q="+location+"&APPID="+apikey+ "&units=metric").json()
+            if weather_data["cod"] == 200:
+                bot.send_message(channel, "The weather in {} is {} and {} degrees.".format(weather_data["name"], weather_data["weather"][0]["description"], weather_data["main"]["temp"]))
+            else:
+                bot.send_message(channel, sender + ': API Fault')
+        else:
+            bot.send_message(channel, "Usage: !weather Istanbul")
 
 bot.on_connect.append(on_connect)
 bot.on_welcome.append(on_welcome)
