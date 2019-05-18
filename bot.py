9e6196849c2a5efe4de5d06815be8f3058f83c1b
RhinosF1 <46229976+RhinosF1@users.noreply.github.com>
2019-05-18 10:06:16 +0100

    Set up

--- bot.py
+++ bot.py
@@ -1,1 +1,34 @@
-##PLACE BOT CODE HERE
+import QuIRC
+import random
+import requests
+import re
+import time
+import random
+topic = "General Chat - Logged at http://wm-bot.wmflabs.org/logs/%23%23RhinosF1/?C=M;O=D , Enjoy - if you don't, have a good one | Rules and Info - https://pastebin.com/C2brE9bh"
+nick = quirctest123
+bot = QuIRC.IRCConnection()
+greetings = [
+    "Hello {}!",
+    "Hi {}!",
+    "Hello there {}!",
+    "Hi there {}!",
+    "Hey {}!"
+]
+
+def on_connect(bot):
+    bot.set_nick(nick)
+    bot.send_user_packet(nick)
+
+def on_welcome(bot):
+    bot.send_message('NickServ', 'identify ')
+    print('Authed to NickServ')
+    time.sleep(10)
+def on_message(bot, channel, sender, message):
+    print('message recieved')
+
+bot.on_connect.append(on_connect)
+bot.on_welcome.append(on_welcome)
+bot.on_public_message.append(on_message)
+
+bot.connect("chat.freenode.net")
+bot.run_loop()
