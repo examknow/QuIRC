ac6cfd02b67bc89be81d8ce7963930de3bea4068
RhinosF1 <46229976+RhinosF1@users.noreply.github.com>
2019-08-21 16:50:53 +0100

    bug fix

--- bot.py
+++ bot.py
@@ -210,11 +210,11 @@ def on_message(bot, channel, sender, message):
         bot.set_nick(nick)
     if message.lower().startswith('!wmca') and cashortbot == 1:
         user = message.split(' ')
-        user = user[2]
+        user = user[1]
         bot.send_message(channel, sender + ': https://meta.wikimedia.org/wiki/Special:CentralAuth/' + user)
     if message.lower().startswith('!mhca') and cashortbot == 1:
         user = message.split(' ')
-        user = user[2]
+        user = user[1]
         bot.send_message(channel, sender + ': https://meta.miraheze.org/wiki/Special:CentralAuth/' + user)
         
         
@@ -326,11 +326,11 @@ def on_pm(bot, sender, message):
         bot.set_nick(nick)
     if message.lower().startswith('wmca') and cashortbot == 1:
         user = message.split(' ')
-        user = user[2]
+        user = user[1]
         bot.send_message(sender, sender + ': https://meta.wikimedia.org/wiki/Special:CentralAuth/' + user)
     if message.lower().startswith('mhca') and cashortbot == 1:
         user = message.split(' ')
-        user = user[2]
+        user = user[1]
         bot.send_message(sender, sender + ': https://meta.miraheze.org/wiki/Special:CentralAuth/' + user)
     
 getinfo()
