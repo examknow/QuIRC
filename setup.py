c1d8b868b9f4f8083510e6eb3c3c7072a67f693e
RhinosF1 <46229976+RhinosF1@users.noreply.github.com>
2019-08-21 12:55:32 +0100

    remove examknow per https://github.com/quirc-bot/QuIRC/issues/58 (#59)
    Barely contributed, no longer staff
    
    fixes #58

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     py_modules = ["QuIRC"],
     version = "BetaV1",
     description = "Quick auto-logging IRC Bot",
-    author = "RhinosF1 and Dschultz75",
+    author = "RhinosF1",
     author_email = "quircbot@gmail.com"
     download_url = "",
     keywords = ["irc"]
