e5e342a736d085448fb5ab262a1055032daaa6fc
RhinosF1 <46229976+RhinosF1@users.noreply.github.com>
2019-08-21 12:41:20 +0100

    update
    remove examknow and increment to v1.1

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup
 setup(
     name = "QuIRC",
     py_modules = ["QuIRC"],
-    version = "BetaV1",
+    version = "BetaV1.1",
     description = "Quick auto-logging IRC Bot",
-    author = "RhinosF1 and Dschultz75",
+    author = "RhinosF1",
     author_email = "quircbot@gmail.com"
     download_url = "",
     keywords = ["irc"]
